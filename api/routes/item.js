d1d7ebd5fe97850bf67fcccd7e376a76b1333bc5
simonhhen <simon_henderson@rogers.com>
2021-04-25 20:38:51 -0700

    first commit attempt

---
+++ api/routes/item.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (app) => {
+  const itemList = require('../controllers/item');
+  app.route('/items')
+    .get(itemList.list_all_items)
+    .post(itemList.create_a_item)
+    .delete(itemList.delete_all_items);
+  app.route('/items/:itemId')
+    .get(itemList.read_a_item)
+    .put(itemList.update_a_item)
+    .delete(itemList.delete_a_item);
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+app = express()
+port = process.env.PORT || 3000
+mongoose = require('mongoose')
+const uri = 'mongodb://localhost/Itemdb';
+Item = require('./api/models/item')
+Contribution = require('./api/models/contribution')
+// mongoose instance connection url connection
+mongoose.connect(uri, {
+ useNewUrlParser: true,
+ useCreateIndex: true,
+ useUnifiedTopology: true
+ }).then(res => {
+  console.log("DB Connected!")
+ }).catch(err => {
+  console.log(Error, err.message);
+})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+// Add headers
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', null);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+const itemRoutes = require('./api/routes/item');
+itemRoutes(app);
+
+const contributionRoutes = require('./api/routes/contribution');
+contributionRoutes(app);
+
+app.get('*', (req, res) => {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+})
+app.listen(port);
+console.log('API server started on: ' + port);
