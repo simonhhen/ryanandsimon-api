7767a2028db0c838d18baebdbd652af280f12af6
simonhhen <simon_henderson@rogers.com>
2021-04-29 11:44:01 -0700

    remove deletes

--- api/routes/contribution.js
+++ api/routes/contribution.js
@@ -4,9 +4,7 @@ module.exports = (app) => {
   app.route('/contributions')
     .get(contributions.list_all_contributions)
     .post(contributions.create_a_contribution)
-    .delete(contributions.delete_all_contributions);
   app.route('/contributions/:contributionId')
     .get(contributions.read_a_contribution)
     .put(contributions.update_a_contribution)
-    .delete(contributions.delete_a_contribution);
 };
